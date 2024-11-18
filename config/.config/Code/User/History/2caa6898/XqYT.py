76a9fb4de3c73ee3256e6231f3aefee4161a3281
Felipe <felipemarques.computacao@gmail.com>
2024-11-18 18:23:45 -0300

    new config file

---
+++ config/.config/Code/User/History/2caa6898/XqYT.py
@@ -0,0 +1,198 @@
+from __future__ import annotations
+from typing import TypeVar, Iterable, Sequence, Generic, List, Callable, Set, Deque, Dict, Any, Optional, Protocol
+from heapq import heappush, heappop
+
+T = TypeVar('T')
+
+
+def linear_contains(iterable: Iterable[T], key: T) -> bool:
+    for item in iterable:
+        if item == key:
+            return True
+    return False
+
+
+C = TypeVar("C", bound="Comparable")
+
+
+class Comparable(Protocol):
+    def __eq__(self, other: Any) -> bool:
+        ...
+
+    def __lt__(self: C, other: C) -> bool:
+        ...
+
+    def __gt__(self: C, other: C) -> bool:
+        return (not self < other) and self != other
+
+    def __le__(self: C, other: C) -> bool:
+        return self < other or self == other
+
+    def __ge__(self: C, other: C) -> bool:
+        return not self < other
+
+
+def binary_contains(sequence: Sequence[C], key: C) -> bool:
+    low: int = 0
+    high: int = len(sequence) - 1
+    while low <= high:  # while there is still a search space
+        mid: int = (low + high) // 2
+        if sequence[mid] < key:
+            low = mid + 1
+        elif sequence[mid] > key:
+            high = mid - 1
+        else:
+            return True
+    return False
+
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+    
+    @property
+    def empty(self) -> bool:
+        return not self._container
+    
+    def push(self, item: T) -> None:
+        self._container.append(item)
+    
+    def pop(self) -> T:
+        return self._container.pop() #LIFO
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+    
+
+class Node(Generic[T]):
+    def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0) -> None:
+        self.state: T = state
+        self.parent: Optional[Node] = parent
+        self.cost: float = cost
+        self.heuristic: float = heuristic
+    
+    def __lt__(self, other: Node) -> bool:
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+
+
+def dfs(initial: T, goal_test: Callable[[T], bool], sucessors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    # frontier corresponde aos lugares que ainda nao visitamos
+    frontier: Stack[Node[T]] = Stack()
+    frontier.push(Node(initial, None))
+    # explored representa os lugares em que já estivemos
+    explored: Set[T] = {initial}
+
+    # continua enquanto houver mais lugares para explorar
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # se encontrarmos o objetivo, terminamos
+        if goal_test(current_state):
+            return current_node
+        # verifica para onde podemos ir em seguida e que ainda não tenha sido explorado
+        for child in sucessors(current_state):
+            if child in explored: # ignora os filhos que ja tenham sido explorados
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None # passamos por todos os lugares e nao atingimos o objetivo
+
+
+def node_to_path(node: Node[T]) -> List[T]:
+    path: List[T] = [node.state]
+    # trabalha o sentido inverso, do final para o inicio
+    while node.parent is not None:
+        node = node.parent
+        path.append(node.state)
+    path.reverse()
+    return path
+
+
+class Queue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: Deque[T] = Deque()
+    
+    @property
+    def empty(self) -> bool:
+        return not self._container # negação é verdadeira para um container vazio
+    
+    def push(self, item: T) -> None:
+        self._container.append(item)
+    
+    def pop(self) -> T:
+        return self._container.popleft() #FIFO
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+ 
+
+def bfs(initial: T, goal_test: Callable[[T], bool], sucessors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    # frontier corresponde aos lugares que ainda nao visitamos
+    frontier: Queue[Node[T]] = Queue()
+    frontier.push(Node(initial, None))
+    # explored representa os lugares em que já estivemos
+    explored: Set[T] = {initial}
+
+    # continua enquanto houver mais lugares para explorar
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # se encontrarmos o objetivo, terminamos
+        if goal_test(current_state):
+            return current_node
+        # verifica para onde podemos ir em seguida e que ainda não tenha sido explorado
+        for child in sucessors(current_state):
+            if child in explored: # ignora os filhos que ja tenham sido explorados
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None # passamos por todos os lugares e nao atingimos o objetivo
+
+
+class PriorityQueue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+    
+    @property
+    def empty(self) -> bool:
+        return not self._container # negação é verdadeira para um container vazio
+    
+    def push(self, item: T) -> None:
+        heappush(self._container, item)
+
+    def pop(self) -> T:
+        return heappop(self._container)
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+ 
+
+def astar(initial: T, goal_test: Callable[[T], bool], sucessors: Callable[[T], List[T]], heuristic: Callable[[T], float]) -> Optional[Node[T]]:
+    # frontier corresponde aos lugares que ainda devemos visitar
+    frontier: PriorityQueue[Node[T]] = PriorityQueue()
+    initial_heuristic = heuristic(initial)
+    print(f"Valor da heurística para o estado inicial: {initial_heuristic}")
+    frontier.push(Node(initial, None, 0.0, initial_heuristic))
+
+    # explored representa os lugres em que já estivemos
+    explored: Dict[T, float] = {initial: 0.0}
+
+    # continua enquanto houver mais lugares para explorar
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # se encontrarmos o objetivo, terminamos
+        if goal_test(current_state):
+            return current_node
+        # verifica onde podemos ir em seguida e que ainda não tenha sido explorado
+        for child in sucessors(current_state):
+            new_cost: float = current_node.cost + 1 # 1 supoe uma grade,
+                                                    # é necessário uma função de custo para aplicações mais sofisticadas
+            if child not in explored or explored[child] > new_cost:
+                explored[child] = new_cost
+                frontier.push(Node(child, current_node, new_cost, heuristic(child)))
+    return None # passamos por todos os lugares e não atingimos o objetivo
+
+
+if __name__ == "__main__":
+    print(binary_contains([2, 4, 5, 8, 12, 28, 54, 99], 4))
---
+++ config/.config/Code/User/History/e285e82/OdfO.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+# ================================================
+# Script de Automação para Instalação no Arch Linux
+# ================================================
+
+
+# Criar diretório para armazenar arquivos temporários
+mkdir -p $WORKDIR
+cd $WORKDIR
+
+# Exibe uma mensagem de boas-vindas
+echo "Iniciando a configuração automática do Arch Linux..."
+
+# Atualizar o sistema
+echo "Atualizando o sistema..."
+sudo pacman -Syu --noconfirm
+
+#Instalar o Yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Instalar Pacotes Essenciais
+echo "Instalando pacotes essenciais..."
+sudo pacman -S --noconfirm \
+  base-devel \
+  git \
+  vim \
+  zsh \
+  curl \
+  htop \
+  neofetch \
+  kitty \
+  xorg-server \
+  xorg-apps \
+  bspwm \
+  thunar \
+  tlp \
+  sxhkd \
+  lightdm lightdm-gtk-greeter
+
+# Configurar o TLP
+echo "Ativando o TLP..."
+sudo systemctl enable tlp
+sudo systemctl start tlp
+
+echo "Substituindo o arquivo de configuração do TLP..."
+sudo cp ~/dotfiles/tlp/tlp /etc/default/tlp
+# ================================================
+# Instalar o Oh My Zsh
+# ================================================
+echo "Instalando Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Definir o Zsh como shell padrão
+chsh -s $(which zsh)
+
+# ================================================
+# Instalar o LightDM (Login Manager)
+# ================================================
+echo "Configurando o LightDM como gerenciador de login..."
+sudo systemctl enable lightdm
+sudo systemctl start lightdm
+
+# ================================================
+# Instalar o navegador Firefox
+# ================================================
+echo "Instalando o Firefox..."
+sudo pacman -S --noconfirm firefox
+
+# ================================================
+# Instalar o editor de texto Visual Studio Code
+# ================================================
+echo "Instalando o Visual Studio Code..."
+yay -S --noconfirm visual-studio-code-bin
+
+# ================================================
+# Instalar outros pacotes adicionais (opcional)
+# ================================================
+echo "Instalando pacotes adicionais..."
+sudo pacman -S --noconfirm \
+  vlc \
+  gimp \
+  kitty \
+  flameshot \
+  i3lock \
+  feh \
+  firefox \
+  picom \
+  pavucontrol \
+  rofi \
+  tumbler \
+  pfetch \
+  fastfetch \
+  xrandr \
+  brightnessctl \
+  zoxide \
+  fzf \
+  audacity \
+  skypeforlinux \
+  bluez \
+  bluez-utils \
+  blueberry 
+
+# Clonando o Repositório de Dotfiles
+echo "Clonando os dotfiles..."
+git clone https://github.com/SEU_USUARIO/dotfiles.git $HOME/dotfiles
+
+# Configuração do dotfiles
+echo "Criando links simbólicos para os dotfiles..."
+
+# Apagando pastas conflitantes
+rm -rf ~/.config 
+rm -rf ~/.zshrc
+rm -rf ~/.oh-my-zsh
+rm -rf .gitconfig
+
+# Criando Links Simbólicos para os Dotfiles
+ln -s $HOME/dotfiles/config $HOME/.config
+ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/oh-my-zsh $HOME/.oh-my-zsh
+ln -s $HOME/dotfiles/git $HOME/.gitconfig
+
+
+
+
+# ================================================
+# Finalizando
+# ================================================
+echo "Configuração concluída! Por favor, reinicie o sistema."
+
